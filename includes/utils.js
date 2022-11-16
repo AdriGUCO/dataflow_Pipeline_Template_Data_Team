5f4694acaf7b4527dfdaa21504191be48b741095
Adria Guerrero <a.guerrero@altostratus.es>
2022-11-16 10:27:03 +0000

    2nd commit

--- includes/utils.js
+++ includes/utils.js
@@ -1,4 +1,5 @@
-/* Snipets de codigo SQL reutilizables */
+/* Snipets de codigo Java y/o SQL reutilizables */
+/* https://cloud.google.com/dataform/docs/reuse-code-includes */
 
 function ifNullZero(parameter){
     return `if( ${parameter} is null, 0, ${parameter} )`;
