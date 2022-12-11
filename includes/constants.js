63ab2c73db38f4a706f9b40e11dfdad0b2e9aeba
Adria Guerrero <a.guerrero@altostratus.es>
2022-12-11 17:02:30 +0000

    pipeline upload

--- includes/constants.js
+++ includes/constants.js
@@ -6,24 +6,17 @@ const DS_STAGING = "dataform_template_datateam_2";
 
 const DS_TRANSFORMATIONS = "dataform_template_datateam_2";
 
-const DS_DESTINATION = "dataform_template_datateam_2";
+const DS_BI = "dataform_template_datateam_2";
 
 const DS_ASSERTIONS = "dataform_template_datateam_2"
 
 const PARTITION_FILTER = "TIMESTAMP_SUB(TIMESTAMP_TRUNC(CURRENT_TIMESTAMP(), HOUR), interval 2 HOUR)";
 
-const PARTITION_FILTER_DAILY = "TIMESTAMP_SUB(TIMESTAMP_TRUNC(CURRENT_TIMESTAMP(), DAY), interval 2 DAY)";
-
-// const PARTITION_FILTER = "TIMESTAMP("2019-01-01")";
-
-// const PARTITION_FILTER_DAILY = "TIMESTAMP("2019-01-01")";
-
 module.exports = {
   PROJECT_ID,
   DS_STAGING,
   DS_TRANSFORMATIONS,
-  DS_DESTINATION,
+  DS_BI,
   DS_ASSERTIONS,
   PARTITION_FILTER,
-  PARTITION_FILTER_DAILY
 };
